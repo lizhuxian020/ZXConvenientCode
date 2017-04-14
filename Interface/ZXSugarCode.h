4cf814bc3231d0b0bfbe0fdac3bdc0d0919517c8
李主贤 <lizhuxian020@pingan.com.cn>
2017-04-14 10:05:06 +0800

    add screen w & h marco

--- Interface/ZXSugarCode.h
+++ Interface/ZXSugarCode.h
@@ -12,4 +12,7 @@
 #import "UIView+ZXSugar.h"
 #import "NSString+common.h"
 
+#define SCREEN_WIDTH [UIScreen mainScreen].bounds.size.width
+#define SCREEN_HEIGHT [UIScreen mainScreen].bounds.size.height
+
 #endif /* ZXSugarCode_h */
