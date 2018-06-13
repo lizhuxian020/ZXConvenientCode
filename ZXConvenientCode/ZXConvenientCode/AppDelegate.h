9357af04c4fcc41a0c73e2f1d0457c62cec28752
Lee_zx <Lee-zx@tom.com>
2018-06-13 14:56:52 +0800

    [CC][Xcode][M][C:lzx] 添加 Xcode 项目以调试用

---
+++ ZXConvenientCode/ZXConvenientCode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZXConvenientCode
+//
+//  Created by forzqy on 2018/6/13.
+//  Copyright © 2018 forzqy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZXConvenientCode/ZXConvenientCode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZXConvenientCode
+//
+//  Created by forzqy on 2018/6/13.
+//  Copyright © 2018 forzqy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
